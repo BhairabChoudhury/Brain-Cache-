9bb3b10c576bc96d0ed8d8e98053c03cdc5c73d0
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-03-26 01:20:13 +0530

    set

--- Backend/src/Config/db.ts
+++ Backend/src/Config/db.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
- 
+import dotenv from "dotenv" ; 
+dotenv.config() ;  
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://Rocky:8101866244@cluster0.ey9q8vn.mongodb.net/Brainly-Project") 
+    await mongoose.connect(process.env.MONGO_URL as string)   // for typescipt , this is  a string 
 .then(()=>console.log("Mongo Db successfully Conected "))
 .catch(err=>console.log("error catch",err)) ;   
 } 
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express" 
 import mongoose from "mongoose" 
 import connectDB from "./Config/db";
+import authroutes from "./routes/authroutes" ; 
 const app = express() ; 
 
 app.use(express.json()) ;
 connectDB() ; 
+console.log("App is running") ; 
+app.use("/api/v1/",authroutes) ; 
+
 
 
 
