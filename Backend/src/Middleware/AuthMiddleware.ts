73a4624a995414e17ae8ca435f4d002f16121400
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-03-27 03:02:45 +0530

    set

--- Backend/src/Middleware/AuthMiddleware.ts
+++ Backend/src/Middleware/AuthMiddleware.ts
@@ -1,1 +1,25 @@
 import express , {NextFunction , Request , Response } from 'express' ; 
+import jwt from  "jsonwebtoken" ; 
+// const JWT_TOKEN = process.env.JWT_TOKEN as string ; 
+
+// export const userMiddleware = async (req:Request,res:Response,next:NextFunction)=>{
+    
+const JWT_TOKEN = process.env.JWT_TOKEN  as string ; 
+
+export  const UserMiddleware = async (req : Request , res : Response , next : NextFunction )=>{
+
+     const token = req.headers["Authorization"] as string ;
+      if(!token) {
+         return  res.status(401).json({"message" :"Unothorized User"})
+      }
+    try{
+        const decoded = jwt.verify(token, JWT_TOKEN) ;
+        //@ts-ignore
+        req.userId = decoded.id  ;
+        next() ;
+        
+    }catch(err){
+        console.log(err) ; 
+        res.status(401).json({"message" :"Unothorized User"})
+    }
+}
