cc220f481ce1ad5868ae4c90dff760f8a77c20a5
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-04-22 00:47:15 +0530

    set

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,1 @@
 
-import './App.css'
-import Dashboard from "./pages/Dashboard";; // Importing the Dashboard page component 
-import { Signup } from './Pages/Signup';
-import { Signin } from './pages/Signin';
-import { BrowserRouter ,Routes ,Route  } from 'react-router-dom';
-// function App() {
-
-
-
-//   return  <BrowserRouter>
-//   <Routes>
-
-
-  
-export default App
-
--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
 
         setLoading(true) ; 
         try {
-      const response = await axios.post("http://localhost:3000/api/auth/signup", {
+      const response = await axios.post("http://localhost:5000/api/auth/signup", {
         name: name,
         email: email,
         password: password
