e6faf352d427411e5d1e323e10dcb8f8322b6e9a
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-06-19 01:03:07 +0530

    set

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,7 +12,7 @@ const app = express() ;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:5174",
+  "https://brain-cache-mu.vercel.app/",
   process.env.FRONTEND_URL
 ].filter(Boolean) as string[];
 
