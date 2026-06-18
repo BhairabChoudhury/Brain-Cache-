6989f826a634157ed90b614a90e5d117f60f70a2
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-06-18 16:10:00 +0530

    set

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import ContentProvider from './Context/ContentProvider';
+//import ContentProvider from './Context/ContentProvider';
 createRoot(document.getElementById('root')!).render( 
    <StrictMode>  
    <BrowserRouter>
